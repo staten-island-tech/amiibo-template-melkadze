c5642997757b73e97f2818e040290306a56a40a1
melkadze <25963683+melkadze@users.noreply.github.com>
2019-12-16 11:44:44 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,6 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.listen(3000, (req, res) => {
+app.get("", (req, res) => {
+  res.send("I likes bagels and butter.");
+});
+
+app.get("/todo", (req, res) => {
+  res.send("Go here: https://medium.com/pan-labs/dynamic-web-apps-on-github-pages-for-free-ffac2b776d45");
+});
+
+app.listen(3000, () => {
     console.log('Listening on port 3000...')
 });
